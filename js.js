d2306d2e2604a02cead89092b3d8819c2fc963fc
Настя Форс <nastafors@MacBook-Pro-Nasta.local>
2025-01-13 18:16:15 +0300

    добавила картинку

--- js.js
+++ js.js
@@ -4,4 +4,5 @@ const button = document.querySelector('button');
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
 	alert('Счастье не в деньках,счастье в их возможности')
+	<img src= "cat.jpg">
 })
